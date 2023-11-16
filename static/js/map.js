fca5bf6f4e68b506f070bfe4e88aaf59517af792
hunterad93 <129822878+hunterad93@users.noreply.github.com>
2023-11-16 14:56:08 -0700

    commit before jquery inclusion

--- static/js/map.js
+++ static/js/map.js
@@ -1,6 +1,65 @@
+// Constants
+const SIDEBAR_WIDTH = '300px';
+const TILE_LAYER_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+function initializeMap(mapId, coordinates, zoomLevel) {
+    var southWest = L.latLng(46.6, -113.0),
+    northEast = L.latLng(47.2, -115.0),
+    bounds = L.latLngBounds(southWest, northEast);
+    var map = L.map(mapId, {
+        maxZoom: 19,
+        minZoom: 10,
+        zoomControl: false
+    }).setView(coordinates, zoomLevel);
+    map.setMaxBounds(bounds);
+    L.tileLayer(TILE_LAYER_URL,{
+        bounds:bounds,
+        maxZoom: 19,
+        minZoom: 10
+    }).addTo(map);
+    return map;
+}
+
+// Sidebar object
+var Sidebar = {
+    element: document.getElementById('sidebar'),
+
+    show: function() {
+        this.element.style.width = SIDEBAR_WIDTH;
+    },
+
+    hide: function() {
+        this.element.style.width = '0';
+    },
+
+    displayProperties: function(propertiesArray) {
+        var content = `<strong><p>${propertiesArray.length} permit(s) at this location</p></strong><table>`;
+        propertiesArray.forEach(properties => {
+            for (var key in properties) {
+                content += `<tr><td><strong>${key}:</strong></td><td>${properties[key]}</td></tr>`;
+            }
+            content += '<tr><td colspan="2"><hr></td></tr>'; // Horizontal line to separate different properties
+        });
+        content += '</table>';
+        this.element.innerHTML = content;
+        this.show();
+    }
+};
+
+// Add event listener to stop propagation of click event in the sidebar
+Sidebar.element.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+
+// Add event listener to hide the sidebar when clicking outside of it
+document.addEventListener('click', function() {
+    Sidebar.hide();
+});
+
 // Initialize map and markers
-// Initialize map at coordinates 46.8721° N, 113.9940° W with a zoom level of 12
-var map = initializeMap('map', [46.8721, -113.9940], 12);
+var map = initializeMap('map', [46.8721, -113.9940], 14);
+
+
 var markers = createMarkerClusterGroup();
 var searchResults = createMarkerClusterGroup();
 
@@ -16,11 +75,7 @@ document.getElementById('search-button').addEventListener('click', function() {
 addClusterClickListener(markers);
 addClusterClickListener(searchResults);
 
-function initializeMap(mapId, coordinates, zoomLevel) {
-    var map = L.map(mapId).setView(coordinates, zoomLevel);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map);
-    return map;
-}
+
 
 function createMarkerClusterGroup() {
     return L.markerClusterGroup({
@@ -62,23 +117,16 @@ function createGeoJsonLayer(data) {
 
 function createMarker(feature, latlng) {
     var marker = L.marker(latlng);
-    marker.on('click', function() {
+    marker.on('click', function(event) {
+        event.originalEvent.stopPropagation();
         displayPropertiesInSidebar(feature.properties);
     });
     return marker;
 }
 
-function displayPropertiesInSidebar(properties) {
-    var table = '<table>';
-    for (var key in properties) {
-        table += `<tr><td>${key}</td><td>${properties[key]}</td></tr>`;
-    }
-    table += '</table>';
-    document.getElementById('sidebar').innerHTML = table;
-}
-
 function searchLocations(markers, searchResults) {
-    // Get the search term
+    // Get the selected property and the search term
+    var property = document.getElementById('property-select').value;
     var searchTerm = document.getElementById('search-input').value.toLowerCase();
 
     // If the search term is empty, show the original markers layer
@@ -101,9 +149,10 @@ function searchLocations(markers, searchResults) {
 
     // Loop through markers
     for (var i = 0; i < allMarkers.length; i++) {
-        // If marker's field contains the search term, add to results
-        var street1 = allMarkers[i].feature.properties.street1;
-        if (street1 && street1.toLowerCase().includes(searchTerm)) {
+        // If marker's selected property contains the search term, add to results
+        var propValue = allMarkers[i].feature.properties[property];
+        
+        if (propValue && propValue.toLowerCase().includes(searchTerm)) {
             searchResults.addLayer(allMarkers[i]);  // Add the marker to the search results
         }
     }
@@ -122,6 +171,7 @@ function searchLocations(markers, searchResults) {
 
 function addClusterClickListener(markerGroup) {
     markerGroup.on('clusterclick', function (a) {
+        a.originalEvent.stopPropagation();
         if (map.getZoom() === map.getMaxZoom()) {
             displayClusterPropertiesInSidebar(a.layer.getAllChildMarkers());
         }
@@ -129,26 +179,18 @@ function addClusterClickListener(markerGroup) {
 }
 
 function displayPropertiesInSidebar(properties) {
-    var table = '<table>';
-    for (var key in properties) {
-        table += `<tr><td>${key}</td><td>${properties[key]}</td></tr>`;
-    }
-    table += '</table>';
-    var sidebar = document.getElementById('sidebar');
-    sidebar.innerHTML = table;
-    sidebar.style.width = '300px';  // Expand the sidebar
+    Sidebar.displayProperties([properties]);
 }
 
 function displayClusterPropertiesInSidebar(markers) {
-    var table = '<table>';
-    markers.forEach(marker => {
-        var row = marker.feature.properties;
-        for (var key in row) {
-            table += `<tr><td>${key}</td><td>${row[key]}</td></tr>`;
-        }
+    var propertiesArray = markers.map(marker => marker.feature.properties);
+    Sidebar.displayProperties(propertiesArray);
+}
+
+var toggleButtons = document.querySelectorAll('.toggle-button');
+for (var i = 0; i < toggleButtons.length; i++) {
+    toggleButtons[i].addEventListener('click', function() {
+        var target = document.getElementById(this.dataset.target);
+        target.classList.toggle('open');
     });
-    table += '</table>';
-    var sidebar = document.getElementById('sidebar');
-    sidebar.innerHTML = table;
-    sidebar.style.width = '300px';  // Expand the sidebar
 }
